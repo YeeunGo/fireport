9a708d61908cc386433913352400d5c41f3baae1
Yeeun GO <yeeun2993@naver.com>
2025-04-09 22:38:51 +0900

    20250409

--- fireport/src/main/java/com/finan/fireport/exception/BadRequestException.java
+++ fireport/src/main/java/com/finan/fireport/exception/BadRequestException.java
@@ -1,15 +1,10 @@
 package com.finan.fireport.exception;
 
 public class BadRequestException extends RuntimeException {
-
     private final ErrorCode errorCode;
 
     public BadRequestException(ErrorCode errorCode, String message) {
         super(message);
         this.errorCode = errorCode;
-    }
-
-    public ErrorCode getErrorCode() {
-        return errorCode;
     }
 }
