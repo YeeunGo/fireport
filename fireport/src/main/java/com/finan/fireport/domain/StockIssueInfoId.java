fae4de598ecb888cfc1e04de465bce9260f4258e
yego <yeeun2993@naver.com>
2025-05-20 07:29:47 +0900

    StockIssueInfo

--- fireport/src/main/java/com/finan/fireport/domain/StockIssueInfoId.java
+++ fireport/src/main/java/com/finan/fireport/domain/StockIssueInfoId.java
@@ -2,13 +2,23 @@ package com.finan.fireport.domain;
 
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+
 
 @Getter
 @Embeddable
 public class StockIssueInfoId  implements Serializable {
     private LocalDate basDt;             // 기준일자
     private String crno;                 // 법인등록번호
+
+    public void setCrno(String crno) {
+        this.crno = crno;
+    }
+
+    public void setBasDt(LocalDate basDt) {
+        this.basDt = basDt;
+    }
 }
--- fireport/src/main/java/com/finan/fireport/mapper/StockIssueInfoMapper.java
+++ fireport/src/main/java/com/finan/fireport/mapper/StockIssueInfoMapper.java
@@ -10,10 +10,15 @@ import java.util.List;
 @Mapper(componentModel="spring", uses = DateMapper.class)
 public interface  StockIssueInfoMapper {
 
-    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "id.crno", source = "crno")
+    @Mapping(target = "id.basDt", source = "basDt")
     StockIssueInfo toEntity(StockIssueInfoResponse dto);
 
+    @Mapping(target = "crno", source = "id.crno")
+    @Mapping(target = "basDt", source = "id.basDt")
     StockIssueInfoResponse toDto(StockIssueInfo entity);
 
+    @Mapping(target = "id.crno", source = "crno")
+    @Mapping(target = "id.basDt", source = "basDt")
     List<StockIssueInfo> toEntityList(List<StockIssueInfoResponse> dtoList);
 }
